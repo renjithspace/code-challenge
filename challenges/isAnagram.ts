05127d292a40addad1feba0899d5d9f6f670c857
renjithspace <renjithrajeevvk@gmail.com>
2023-05-05 21:13:52 +0530

    Improve documentation

--- challenges/isAnagram.ts
+++ challenges/isAnagram.ts
@@ -9,23 +9,34 @@
  * Space complexity O(1)
  */
 export function isAnagram(firstWord: string, secondWord: string) {
+  // If two words length different then it's not anagram
   if (firstWord.length !== secondWord.length) return false;
+
+  // Convert the words to lower case for case insensitivity
   firstWord = firstWord.toLowerCase();
   secondWord = secondWord.toLowerCase();
+
+  // Create an array of characters count of first word
   const firstWordCharCodes = new Array(26).fill(0);
   for (let index = 0; index < firstWord.length; index++) {
     const charCodeIndex = firstWord.charCodeAt(index) - 97;
     firstWordCharCodes[charCodeIndex]++;
   }
+
+  // Create an array of characters count of second word
   const secondWordCharCodes = new Array(26).fill(0);
   for (let index = 0; index < secondWord.length; index++) {
     const charCodeIndex = secondWord.charCodeAt(index) - 97;
     secondWordCharCodes[charCodeIndex]++;
   }
+
+  // If the characters counts are different, it's not an angram
   for (let index = 0; index < firstWordCharCodes.length; index++) {
     if (firstWordCharCodes[index] !== secondWordCharCodes[index]) {
       return false;
     }
   }
+
+  // Otherwise it's an anagram
   return true;
 }
